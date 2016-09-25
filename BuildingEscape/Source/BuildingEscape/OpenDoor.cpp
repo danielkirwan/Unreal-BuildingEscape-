628dd8aa8ee90d447929a5701b9ec96285f11e37
Dan-PC\Dan <danielkirwan07@gmail.com>
2016-09-25 16:33:49 +0100

    Section 03 _ building escape - lecture 84 - iterating with Tarray over FOR LOOP

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -47,7 +47,7 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
 	// Poll the trigger volume every frame
-	if (GetTotalMassOfActorsOnPlate() > 50.f) //TODO make into a parameter
+	if (GetTotalMassOfActorsOnPlate() > 30.f) //TODO make into a parameter
 	{
 		OpenDoor();
 		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
@@ -66,8 +66,12 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate() {
 		//Find all the overlapping actors
 	TArray<AActor*> OverlappingActors;
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
-		//iterate through them adding their masses
+	//iterate through them adding their masses
 
+	for (const auto& Actor : OverlappingActors) {
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp, Warning, TEXT("%s on Pressure plate"), *Actor->GetName());
+	}
 
 		return TotalMass;
 }
